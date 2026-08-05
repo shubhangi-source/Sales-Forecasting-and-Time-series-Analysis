fa28a22e24be79cd5d86af3775cc1d7d74a715a6
shubhangi singh <shubhangisingh671@gmail.com>
2026-08-05 23:42:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -276,9 +276,7 @@ with tab3:
     # KPI Cards
     # -------------------------
 
-    df = pd.read_csv(
-        r"C:\Users\shubh\OneDrive\Documents\sales analysis and forecasting\featured__new_sales_data.csv"
-    )
+    df = pd.read_csv("data/processed_sales_data.csv")
     c1, c2, c3, c4 = st.columns(4)
 
     c1.metric("💰 Total Sales", f"₹ {df['Sales'].sum():,.0f}")
